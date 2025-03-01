222250bd5718cdb13c848cb96566315540ad40fb
amaldominic816 <amaldominic816@gmail.com>
2025-03-01 10:39:35 +0400

    Updated the  cart and occasionpage

---
+++ ios/lowrztest/lowrztestApp.swift
@@ -0,0 +1,17 @@
+//
+//  lowrztestApp.swift
+//  lowrztest
+//
+//  Created by Flowrz on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct lowrztestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/lowrztestTests/lowrztestTests.swift
@@ -0,0 +1,17 @@
+//
+//  lowrztestTests.swift
+//  lowrztestTests
+//
+//  Created by Flowrz on 28/02/2025.
+//
+
+import Testing
+@testable import lowrztest
+
+struct lowrztestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
